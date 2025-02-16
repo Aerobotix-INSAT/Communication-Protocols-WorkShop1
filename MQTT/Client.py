9420df6b345c5bf1795f0e3b703e0dc4724a7b13
zouaghista <59181866+zouaghista@users.noreply.github.com>
2025-02-16 15:49:38 +0100

    Update Client.py
    Fixed a bug in MQTT client that caused it to never subscribe to the desired topic.

--- MQTT/Client.py
+++ MQTT/Client.py
@@ -8,17 +8,19 @@ subscribe_topic = "iot/test/receive"  # Topic to subscribe to
 publish_topic = "iot/test/receive"  # Topic to send messages to
 
 # Define the callback function to handle received messages
-def on_connect(client, userdata, flags, rc):
-    print(f"Connected to MQTT broker with result code {rc}")
-    # Subscribe to the topic
-    client.subscribe(subscribe_topic)
-
-def on_message(client, userdata, msg):
-    print(f"Received message: {msg.payload.decode()} on topic: {msg.topic}")
 
 def send_message(message):
     client.publish(publish_topic, message)
-    print(f"Sent message: {message} to topic: {publish_topic}")
+    #print(f"Sent message: {message} to topic: {publish_topic}")
+
+def on_connect(client, userdata, flags, rc, x):
+    print(f"Connected to MQTT broker with result code {rc}")
+    # Subscribe to the topic
+    client.subscribe(subscribe_topic)
+    send_message("Hello world")
+
+def on_message(client, userdata, msg):
+    print(f"Received message: {msg.payload.decode()} on topic: {msg.topic}")
 
 # Create an MQTT client instance
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
@@ -26,7 +28,6 @@ client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 # Set the callback functions
 client.on_connect = on_connect
 client.on_message = on_message
-client.on_connect = send_message("hi")
 
 # Connect to the MQTT broker
 client.connect(broker, port, 60)
@@ -37,7 +38,8 @@ client.loop_start()
 # Main loop
 try:
     while True:
-        pass
+        message = input('->')
+        send_message(message)
 except KeyboardInterrupt:
     print("Disconnected from MQTT broker.")
     client.loop_stop()
